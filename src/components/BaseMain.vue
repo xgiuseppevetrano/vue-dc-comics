b211c501049251e81536c05951d1ac5eb13268ff
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-01 17:01:42 +0200

    add main

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,5 +1,9 @@
 <template>
-  
+    <main>
+        <div class="container">
+            <p>Content goes here</p>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -9,5 +13,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    main {
+        background-color: var(--secondary-color);
+        padding: 1.875rem;
+        text-align: center;
+        color: var(--secondary-color-text);
+        font-weight: 500;
+    }
 </style>
