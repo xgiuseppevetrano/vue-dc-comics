c69ef4fb49b00a2c9f24b967b25bdd8c98346cca
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-01 16:00:59 +0200

    add scafolding

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'BaseMain',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
